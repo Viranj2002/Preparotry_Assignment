44ad6ac92c3f14ca17d32aed2ca05a24ab743c92
Viranj Masdeakr <viranjrmasdekar@gmail.com>
2025-02-26 16:21:04 +0530

    assign12 added

---
+++ assignment12.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+void reverseString(char str[]) {
+    int start = 0;
+    int end = strlen(str) - 1;
+    
+    while(start < end){
+        char temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+int main() {
+    char str[100];
+    printf("Enter a string ");
+    fgets(str, sizeof(str),stdin);
+
+    reverseString(str);
+    printf("The reverse string is %s",str);
+    
+    return 0;
+}
